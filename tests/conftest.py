21ed1d0605536339b7b8112a9e46aa45291f6c18
Oliver Bestwalter <oliver.bestwalter@avira.com>
2021-01-31 23:33:38 +0100

    add a fixture to reset the config before every test
    This ensures that changes to the thread local default reporter object and command line options tests will be set to their defaults before each test function.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,1 +1,14 @@
+import pytest
+
+import approvaltests
+
 pytest_plugins = 'pytester'
+
+_DEFAULT_REPORTER = approvaltests.get_default_reporter()
+
+
+@pytest.fixture(autouse=True)
+def reset_approvaltests_config(request):
+    approvaltests.set_default_reporter(_DEFAULT_REPORTER)
+    for option_name in [o for o in vars(request.config.option) if "approvaltests" in o]:
+        setattr(request.config.option, option_name, None)
