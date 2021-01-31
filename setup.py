e6cd5aa01f4b4f1c883b947a9ba7b6fad6228fda
Llewellyn Falco <llewellyn.falco@gmail.com>
2021-01-31 19:22:53 +0000

    v0.2.2
    Co-Authored-By: rzijp <7345541+rzijp@users.noreply.github.com>
    Co-Authored-By: Oliver Bestwalter <263007+obestwalter@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def read(fname):
 
 setup(
     name='pytest-approvaltests',
-    version='0.2.1',
+    version='0.2.2',
     author='Emily Bache',
     author_email='emily@bacheconsulting.com',
     maintainer='Emily Bache',
